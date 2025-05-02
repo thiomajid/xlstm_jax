96e1ab698adb2d47f3f75a88ff19d2e00369ae33
thiomajid <tchiombianomajid@gmail.com>
2025-05-02 20:37:51 +0100

    Refactor main script to load configuration and model from JSON, removing the placeholder main function.

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from xlstm-jax!")
+import json
 
+from flax import nnx
+
+from xlstm_jax import xLSTMLMModel
+from xlstm_jax.utils import load_model_from_checkpoint, parse_xlstm_config_dict
 
 if __name__ == "__main__":
-    main()
+    with open("./outputs/2025-05-02/20-28-10/artifacts/config.json") as f:
+        config_dict = json.load(f)
+    config = parse_xlstm_config_dict(config_dict)
+    model = xLSTMLMModel(config, rngs=nnx.Rngs(0))
+    restored_model = load_model_from_checkpoint(
+        model, checkpoint_path="./outputs/2025-05-02/20-28-10/artifacts/state"
+    )
